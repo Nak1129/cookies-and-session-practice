de3f815aa9ec6fd74d357af875ce56e2d991c684
Nak <leoyang891129@gmail.com>
2023-06-11 14:50:04 +0800

    下載dotenv做資安和使用gitignore

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-app.use(cookieParser("我是秘密String"));
+app.use(cookieParser(process.env.MYCOOKIESECERETKEY));
+app.use(
+  session({
+    secret: process.env.MYSESSIONSECERETKEY,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }, //localhost
+  })
+);
+
+const checkUser = (req, res, next) => {
+  if (!req.session.isVerified) {
+    return res.send("請先登入系統，才能看到秘密。");
+  } else {
+    next();
+  }
+};
 
 app.get("/", (req, res) => {
   return res.send("這是首頁");
@@ -16,6 +34,29 @@ app.get("/setCookie", (req, res) => {
 app.get("/seeCookie", (req, res) => {
   console.log(req.signedCookies);
   return res.send("看設定好的cookie  " + req.signedCookies.yourCookie);
+});
+
+app.get("/setSessionData", (req, res) => {
+  req.session.express = "something not important";
+  return res.send("在伺服器設置session資料，在瀏覽器設置簽名後的session id");
+});
+
+app.get("/seeSessionData", (req, res) => {
+  console.log(req.session);
+  return res.send("看設定好的session資料" + req.session.express);
+});
+
+app.get("/verifyUser", (req, res) => {
+  req.session.isVerified = true;
+  return res.send("你已經被驗證了。。。");
+});
+
+app.get("/secret", checkUser, (req, res) => {
+  return res.send("秘密是，柴犬很可愛。。。");
+});
+
+app.get("/secret2", checkUser, (req, res) => {
+  return res.send("秘密是2，駱駝很可愛。。。");
 });
 
 app.listen(3000, () => {
